5cfa49b31067461ffbf16754c2164cdd63d1b73f
Bismuth083 <147175162+Bismuth083@users.noreply.github.com>
2026-04-25 22:18:54 +0900

    プロジェクトファイルを追加。

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using static TRPGDice.TRPGDice;
+
+namespace TRPGDice;
+
+internal class Program
+{
+  public static void Main(string[] args)
+  {
+    if (args.Length == 0)
+    {
+      Console.WriteLine(HELP);
+      return;
+    }
+
+    args[0] = args[0].ToLower();
+
+    if (args[0] == "-h" | args[0] == "-v" | args[0] == "--help" | args[0] == "--version")
+    {
+      Console.WriteLine(HELP);
+      return;
+    }
+
+    if (args.Length == 1)
+    {
+      Console.WriteLine(Roll(args[0]));
+      return;
+    }
+
+    else
+    {
+      throw new ArgumentException("""
+        コマンドの使い方が誤っています。
+
+        正しい使い方を確認したい場合は、以下のコマンドをお試しください。
+
+        dice -h
+        """);
+    }
+  }
+
+
+  const string HELP = """
+      TRPGDice [Version 1.0.0]
+      
+      使い方: dice [計算式]
+
+      例
+      ----------------
+
+      dice "2d6"          => 6面ダイスを2回振った結果を出力
+      dice "1d100"        => 100面ダイスを1回振った結果を出力
+      dice "1d6 + 3"      => 6面ダイスの結果に3を加えた値を出力
+      dice "abs(1d5 - 2)" => 5面ダイスの結果から2を引き、その絶対値を取った値を出力
+
+      diceの使い方については、以下を参照してください。
+      https://github.com/Bismuth083/AlgorithmPractice/TRPGDice
+
+      計算式に使用可能な要素については、以下を参照してください。
+      https://sys27.github.io/xFunc/articles/supported-functions-and-operations.html
+      """;
+}
+
---
+++ TRPGDice.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using xFunc.Maths;
+
+namespace TRPGDice
+{
+  public static partial class TRPGDice
+  {
+    [GeneratedRegex(@"\d+d\d+")]
+    private static partial Regex NdM();
+
+    /// <summary>
+    /// `"1d6 + 3"`のような表記に従って、ランダムな結果を返します
+    /// </summary>
+    /// <seealso cref="https://sys27.github.io/xFunc/articles/supported-functions-and-operations.html"/>
+    /// <param name="equation">`"1d6 + 3"のように表記された文字列`</param>
+    /// <returns>計算した結果</returns>
+    public static double Roll(string equation)
+    {
+      // "1d100"のように表記された部分を先に処理する
+      string rolled = NdM().Replace(equation, new MatchEvaluator(
+        num => { return RollNdM(num.ToString()).ToString(); })
+        );
+
+      // この時点でただの定数ならば、xFuncの処理を行わずにreturnする
+
+      if (int.TryParse(equation, out int num))
+      {
+        return num;
+      }
+
+      // xFuncを使うために必要な変数
+      var processor = new Processor();
+
+      // Rollされた式を計算して返す
+      try
+      {
+        return processor.Solve(rolled).Number.Number;
+      }
+      catch (Exception) {
+        throw new ArgumentException("""
+          計算式の処理に失敗しました。計算式が正しく表記されているか確認してください。
+
+          使用可能な式については、以下のサイトを参照してください。
+          https://sys27.github.io/xFunc/articles/supported-functions-and-operations.html
+          """);
+      }
+    }
+    /// <summary>
+    /// `"1d100"`のような形式に従ってランダムな結果を返します
+    /// </summary>
+    /// <param name="NdM">`"1d000"`のように表記された文字列</param>
+    /// <returns>サイコロを振った結果</returns>
+    public static int RollNdM(string NdM)
+    {
+      // "1d100"のように表された文字列をdの前後で分割し、
+      // countとsurfaceにそれぞれ格納する
+      string[] split = NdM.Split('d');
+
+      int count = int.Parse(split[0]);
+      int surface = int.Parse(split[1]);
+
+      int sum = 0;
+
+      // countの回数だけ 1 <= X <= surface の範囲で加算を繰り返す
+      for (int i = 0; i < count; i++) {
+        sum += rand.Next(1, 1 + surface);
+      }
+
+      return sum;
+    }
+   static readonly Random rand = new();
+  }
+}
